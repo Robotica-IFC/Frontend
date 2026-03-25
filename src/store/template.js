db2a5b724b7af017fc4a7d0d9de5076e66c8ad81
LauraEloysa <lauraeloysad@gmail.com>
2026-03-25 16:12:59 -0300

    FEAT: Criação da área de redefiir senha #5

--- src/store/template.js
+++ src/store/template.js
@@ -4,4 +4,10 @@ import { ref } from "vue";
 export const useTemplateStore = defineStore('template', () => {
     const sign = ref(0);
     const changePassword = ref(0);
+
+
+    return{
+        sign,
+        changePassword,
+    }
 })
