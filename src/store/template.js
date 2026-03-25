69ec9b638d56249d8b51330ceb21284060d71c81
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2026-03-25 09:58:20 -0300

    FEAT:  Inicio da template store

---
+++ src/store/template.js
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useTemplateStore = defineStore('template', () => {
+    const sign = ref(0);
+    const changePassword = ref(0);
+})
