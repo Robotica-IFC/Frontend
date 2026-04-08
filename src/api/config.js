23a60e29dd4d0ad4a15375cac247ef58183c5bf2
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2026-04-08 11:14:00 -0300

    FIX: Adicionando a url do backend local para não precisar ficar criando a .env

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const url = import.meta.env.VITE_URL_BACKEND
 
 const api = axios.create({
-    baseURL: url
+    baseURL: url ?? 'http://127.0.0.1:8000/api/'
 })
 
 export default api;
