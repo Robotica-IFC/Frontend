1038392f613f42171c023f222319a50ad73420bd
LucasFontini1 <lucasfontinidaluz@gmail.com>
2026-04-22 19:38:01 -0300

    FEAT: Conclusão do style da view de tests, configuração da view de sign e criação dos components para professor

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.d68ndi1hagc"
+    "revision": "0.2n37vpk0lso"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
