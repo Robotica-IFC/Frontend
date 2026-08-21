e8db49470b340f22ce41d928009cbd0a5c57a25a
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2026-08-21 15:59:31 -0300

    FEAT: adicionando o openProject no listProjects

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.v1kneg9qpbk"
+    "revision": "0.r47ddokbbgo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
